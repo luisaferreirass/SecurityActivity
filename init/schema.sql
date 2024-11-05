333536e5b78cc003c8d280b0ef99832cc0326bfb
Luisa <luisaferreirass08@gmail.com>
2024-11-05 13:45:09 -0300

    config: implementing initials configurations

---
+++ init/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS 'users' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    balance FLOAT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'orders' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    date TEXT NOT NULL
+);
+
