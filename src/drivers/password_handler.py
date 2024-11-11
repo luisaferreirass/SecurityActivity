c5ea1c4ee4fac36d2b7e8e56bedd370028719373
Luisa <luisaferreirass08@gmail.com>
2024-11-11 15:24:40 -0300

    feat: correcting errors

--- src/drivers/password_handler.py
+++ src/drivers/password_handler.py
@@ -1,12 +1,12 @@
-import bycript
+import bcrypt
 
 class PasswordHandler:
 
     def encrypt_password(self, password: str) -> str:
-        salt = bycript.gentsalt()
+        salt = bcrypt.gensalt()
 
-        hashed_password = bycript.hashpw(password.encode("utf-8"), salt)
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
         return hashed_password
     
     def check_password(self, password: str, hashed_password: str) -> bool:
-        return bycript.checkpw(password.encode("utf8", hashed_password))
+        return bcrypt.checkpw(password.encode("utf8", hashed_password))
