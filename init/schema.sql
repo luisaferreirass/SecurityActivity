0e06766e11d0cf98a38c025509a564b2bc2d9346
Luisa <luisaferreirass08@gmail.com>
2024-11-11 15:25:09 -0300

    feat: correcting errors

--- init/schema.sql
+++ init/schema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS 'users' (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    balance FLOAT NOT NULL
+    password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS 'orders' (
